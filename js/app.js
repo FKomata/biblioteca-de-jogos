e517a4d515886da7c74d97aadc777289b5bcbff9
Felipe Komata <fbkomata@gmail.com>
2025-04-29 11:06:32 -0300

    funcionalidade visual e funcional ao projeto

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@ function alterarStatus(id){
     let jogoClicado = document.getElementById(`game-${id}`);
     let botao = jogoClicado.querySelector('.dashboard__item__button');
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
-    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name')
         
-    alert(nomeJogo.textContent)
-
+    if(botao.classList.contains('dashboard__item__button--return')) {
+           botao.classList.remove('dashboard__item__button--return') ;
+       }else{
+         botao.classList.add('dashboard__item__button--return');
     }
+
+    if(imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented') ;
+    }else{
+        imagem.classList.add('dashboard__item__img--rented');
+    }
+
+    
+
+}
 
 
    // let texto = document.getElementById('')
@@ -16,10 +27,4 @@ function alterarStatus(id){
    // }else{
    //     botao.classList.add('dashboard__items__item');
     //    botao.classList.remove('dashboard__item__img dashboard__item__img--rented');
-   // }
-
-
-
-
-
-
+   // 
