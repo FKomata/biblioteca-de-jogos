b9c0d4d4218f4a7434fc9f4e13d67d8b65a42d62
Felipe Komata <fbkomata@gmail.com>
2025-04-29 11:16:46 -0300

    melhoria do codigo + mudança do texto do botão

--- js/app.js
+++ js/app.js
@@ -1,30 +1,17 @@
 function alterarStatus(id){
+    
     let jogoClicado = document.getElementById(`game-${id}`);
     let botao = jogoClicado.querySelector('.dashboard__item__button');
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
         
-    if(botao.classList.contains('dashboard__item__button--return')) {
-           botao.classList.remove('dashboard__item__button--return') ;
-       }else{
-         botao.classList.add('dashboard__item__button--return');
-    }
-
     if(imagem.classList.contains('dashboard__item__img--rented')) {
         imagem.classList.remove('dashboard__item__img--rented') ;
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'alugar';
     }else{
         imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'devolver';
     }
 
-    
-
 }
-
-
-   // let texto = document.getElementById('')
-    //    if(botao.classList.contains('dashboard__items__item>')) {
-     //   botao.classList.remove('dashboard__items__item>') ;
-    //    botao.classList.add('dashboard__item__img dashboard__item__img--rented>');
-   // }else{
-   //     botao.classList.add('dashboard__items__item');
-    //    botao.classList.remove('dashboard__item__img dashboard__item__img--rented');
-   // 
