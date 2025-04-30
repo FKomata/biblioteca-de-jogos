490c22b8bd51b112f19c42fc7fbcf015ddf22f7b
Felipe Komata <fbkomata@gmail.com>
2025-04-30 10:28:44 -0300

    adição de confirmação de devolução do jogo

--- js/app.js
+++ js/app.js
@@ -3,15 +3,28 @@ function alterarStatus(id){
     let jogoClicado = document.getElementById(`game-${id}`);
     let botao = jogoClicado.querySelector('.dashboard__item__button');
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
-        
+    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
+    
     if(imagem.classList.contains('dashboard__item__img--rented')) {
+        if(confirm(`você tem certeza que quer devolver o jogo ${nomeJogo.textContent}`)){;
         imagem.classList.remove('dashboard__item__img--rented') ;
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'alugar';
+        }
     }else{
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'devolver';
     }
 
+
+
+}
+
+
+function jogosAlugados(){
+    let alugados = jogoClicado.querySelector('.dashboard__item__button--return');
+    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
+
+    return console.log(nomeJogo.textContent);
 }
